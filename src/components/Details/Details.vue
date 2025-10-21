95b6b0e46f0697b0308516036fc597b02e07e57e
Attilio Capocchiani <acapocchiani@deloitte.it>
2025-10-21 17:39:25 +0200

    Details empty component

---
+++ src/components/Details/Details.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const x: string = ""
+console.log(x);
+</script>
+
+<template>
+    <div class="slds-box slds-theme_default">
+        <div class="slds-tabs_default">
+            <ul class="slds-tabs_default__nav" role="tablist">
+                <li class="slds-tabs_default__item slds-is-active" title="Related" role="presentation">
+                    <a class="slds-tabs_default__link" href="#" role="tab" tabindex="0" aria-selected="true"
+                        aria-controls="tab-default-1" id="tab-default-1__item">Related</a>
+                </li>
+            </ul>
+            <div id="tab-default-1" class="slds-tabs_default__content slds-show" role="tabpanel"
+                aria-labelledby="tab-default-1__item">Related Content</div>
+        </div>
+    </div>
+</template>
