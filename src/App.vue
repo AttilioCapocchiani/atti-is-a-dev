3eea80b7d11fd05134da99501325b62c12768aa2
Attilio Capocchiani <acapocchiani@deloitte.it>
2025-11-04 12:08:45 +0100

    Mobile improvements

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-  import '@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css'
-  import Header from './components/Header/Header.vue';
-  import Details from './components/Details/Details.vue';
-  import Experiences from './components/Experiences/Experiences.vue';
-  import { atti } from './data.ts'
+import '@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css'
+import Header from './components/Header/Header.vue';
+import Details from './components/Details/Details.vue';
+import Experiences from './components/Experiences/Experiences.vue';
+import { atti } from './data.ts'
 </script>
 
 <template>
@@ -17,17 +17,29 @@
       :looking-for-job="atti.lookingForJob"
     />
 
-    <div class="slds-grid slds-m-top--small">
-      <div class="slds-col slds-size--7-of-12 slds-p-right--x-small">
+    <div class="slds-grid slds-wrap slds-p-top--medium">
+      <div class="slds-col slds-small-size--1-of-1 slds-medium-size--1-of-1 slds-large-size--8-of-12">
         <Details
           :atti="atti"
-          :certifications="atti.certifications" />
+          :certifications="atti.certifications"/>
       </div>
-      <div class="slds-col slds-size--5-of-12">
-        <experiences :experiences="atti.jobHistory" />
+      <div class="slds-col slds-small-size--1-of-1 slds-medium-size--1-of-1 slds-large-size--4-of-12 responsive-margin">
+        <experiences :experiences="atti.jobHistory"/>
       </div>
     </div>
   </div>
-  </template>
+</template>
 
-<style scoped></style>
+<style scoped>
+
+.responsive-margin {
+  padding-top: 1rem;
+}
+
+@media (min-width: 48em) {
+  .responsive-margin {
+    padding-top: 0;
+    padding-left: 1rem;
+  }
+}
+</style>
